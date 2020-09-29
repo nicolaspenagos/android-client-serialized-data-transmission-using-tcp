e22498dc702fde52eb3092c1434087a60f405582
Nicolás Penagos <nicolas.penagosm98@gmail.com>
2020-09-29 17:26:33 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android_clientserialized_data_transmission_using_tcp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.android_clientserialized_data_transmission_using_tcp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
